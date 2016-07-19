ae8e59415849047a5cf8127ab96b67e126d8d203
Kyle St-Hill <kylesthill77@gmail.com>
2016-07-19 11:17:06 -0400

    Initial commit

---
+++ app/src/main/java/com/globaltelecomunicationinc/tic_tac_toe/MainActivity.java
@@ -0,0 +1,109 @@
+package com.globaltelecomunicationinc.tic_tac_toe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //0-yellow 1-red
+    int activePlayer = 0;
+
+    boolean gameIsActive = true;
+
+    //unplayed
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningStates = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+
+    public void dropIn(View view){
+        ImageView counter = (ImageView)view;
+
+        //counter.getTag().toString();
+        //System.out.println(counter.getTag().toString());
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if(gameState[tappedCounter] == 2 && gameIsActive){
+            gameState[tappedCounter]= activePlayer;
+            counter.setTranslationY(-1000f);
+            if(activePlayer==0){
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            }else{
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).setDuration(300);
+
+            for(int[] won : winningStates){
+
+                if(gameState[won[0]] == gameState[won[1]] &&
+                        gameState[won[1]] == gameState[won[2]] &&
+                            gameState[won[0]] != 2){
+
+                                //Someone won the game
+                                gameIsActive = false;
+                                String winner = "Red";
+                                if (gameState[won[0]]== 0){
+                                    winner = "Yellow";
+                                }
+                                //System.out.println(gameState[won[0]]);
+                                TextView winnerMessage= (TextView)findViewById(R.id.winnerMessage);
+                                winnerMessage.setText(winner + " has won the game");
+                                Toast.makeText(getApplicationContext(),"Player " + winner + " won!", Toast.LENGTH_LONG).show();
+                                LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+
+                                layout.setVisibility(View.VISIBLE);
+                }else{
+                    boolean gameOver = true;
+
+                    for(int counterState : gameState){
+                        if (counterState == 2){ gameOver = false;}
+                    }
+                    if (gameOver){
+                        TextView winnerMessage= (TextView)findViewById(R.id.winnerMessage);
+                        winnerMessage.setText("DRAW Game over");
+                        Toast.makeText(getApplicationContext(),"Draw!", Toast.LENGTH_LONG).show();
+                        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+
+                        layout.setVisibility(View.VISIBLE);
+
+                    }
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view){
+
+        gameIsActive = true;
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+
+        layout.setVisibility(View.INVISIBLE);
+
+        int activePlayer = 0;
+
+        for(int i = 0 ; i<gameState.length; i++){
+            gameState[i]=2;
+        }
+
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+
+        for(int i =0 ; i<gridLayout.getChildCount();i++){
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
